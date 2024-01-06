0128e0539407ea1be29c5abb67ea17a864d38658
Hector Rivera <contacto.kerikka@gmail.com>
2024-01-06 14:35:16 -0500

    init

---
+++ sendemail.php
@@ -0,0 +1,39 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recopilar datos del formulario
+    $username = $_POST["username"];
+    $phone = $_POST["phone"];
+    $caseType = $_POST["case_type"];
+    $message = $_POST["message"];
+
+    // Destinatario del correo electrónico
+    $to = "contacto.kerikka@gmail.com";
+
+    // Asunto del correo electrónico
+    $subject = "Nuevo formulario de contacto";
+
+    // Mensaje del correo electrónico
+    $email_message = "Nombre: $username\n";
+    $email_message .= "Teléfono: $phone\n";
+    $email_message .= "Tipo de caso: $caseType\n";
+    $email_message .= "Mensaje:\n$message";
+
+    // Cabeceras del correo electrónico
+    $headers = "From: $username <$to>\r\n";
+    $headers .= "Reply-To: $to\r\n";
+
+    // Intentar enviar el correo electrónico
+    if (mail($to, $subject, $email_message, $headers)) {
+        // Éxito al enviar el correo
+        header("Location: confirmacion.html");
+        exit();
+    } else {
+        // Error al enviar el correo
+        echo "Error al enviar el formulario. Por favor, inténtelo de nuevo.";
+    }
+} else {
+    // Redireccionar si se accede directamente a este script sin enviar el formulario
+    header("Location: index.html");
+    exit();
+}
+?>
